8a50fea293ed69971ec6eb5ddfd753cd93c7ae9b
gvmotta <gustavomottadeveloper@gmail.com>
2025-01-13 01:53:15 -0300

    feat: new unit tests implemented

--- src/utils/tests/regexValidations.spec.mjs
+++ src/utils/tests/regexValidations.spec.mjs
@@ -1,27 +1,27 @@
-import testEmail from "../testEmailSyntax.mjs";
+import { testEmailSyntax } from "../testEmailSyntax.mjs";
 
-describe('validarEmail', () => {
-  it('deve retornar true para um email válido', () => {
-    expect(testEmail('teste@exemplo.com')).toBe(true);
+describe("validarEmail", () => {
+  it("deve retornar true para um email válido", () => {
+    expect(testEmailSyntax("teste@exemplo.com")).toBe(true);
   });
 
   it('deve retornar false para um email sem "@"', () => {
-    expect(testEmail('testeexemplo.com')).toBe(false);
+    expect(testEmailSyntax("testeexemplo.com")).toBe(false);
   });
 
-  it('deve retornar false para um email sem domínio', () => {
-    expect(testEmail('teste@')).toBe(false);
+  it("deve retornar false para um email sem domínio", () => {
+    expect(testEmailSyntax("teste@")).toBe(false);
   });
 
-  it('deve retornar false para um email com espaços', () => {
-    expect(testEmail('teste @exemplo.com')).toBe(false);
+  it("deve retornar false para um email com espaços", () => {
+    expect(testEmailSyntax("teste @exemplo.com")).toBe(false);
   });
 
-  it('deve retornar false para um email sem TLD', () => {
-    expect(testEmail('teste@exemplo')).toBe(false);
+  it("deve retornar false para um email sem TLD", () => {
+    expect(testEmailSyntax("teste@exemplo")).toBe(false);
   });
 
   it('deve retornar false para um email com múltiplos "@"', () => {
-    expect(testEmail('teste@@exemplo.com')).toBe(false);
+    expect(testEmailSyntax("teste@@exemplo.com")).toBe(false);
   });
 });
